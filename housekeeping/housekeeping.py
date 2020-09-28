d11abec6bb4c5705f8e27a034ed4d9b505c5adee
Hannah Tomio <tomio@mit.edu>
2020-09-28 09:02:07 -0400

    Add simple housekeeping example

---
+++ housekeeping/housekeeping.py
@@ -0,0 +1,47 @@
+from multiprocessing import Process
+from time import sleep
+
+def Camera():
+    print("Test camera process")
+    sleep(20)
+
+def Fpga():
+    print("Test fpga process")
+    sleep(5)
+
+def Bus():
+    print("Test bus process")
+    sleep(10)
+
+if __name__ == '__main__':
+    tasks = [Camera, Fpga, Bus]
+    processes = {}
+    n = 0
+
+    for task in tasks:
+        p = Process(target=task)
+        p.start()
+        processes[n] = (p, task)
+        n += 1
+
+    while(len(processes) > 0):
+        for n in list(processes):
+            (p, t) = processes[n]
+            if p.exitcode is None: # Not finished
+                if not p.is_alive(): # Not running
+                    print(t, "not finished and not running")
+                    # Do error handling and restarting here assigning the new process to processes[n]
+                else:
+                    print(t, "not finished")
+            elif p.exitcode < 0:
+                print(t, "ended with an error", p.exitcode)
+                # Handle this either by restarting or delete the entry so it is removed from list as for else
+            else:
+                print(t, "finished")
+                p.join() # Allow tidyup
+                del processes[n] # Removed finished items from the dictionary 
+                # When none are left then loop will end
+        sleep(5)
+    print("FINISHED")
+
+
