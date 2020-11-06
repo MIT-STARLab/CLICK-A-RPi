194ca9c95916d1c9129c5f76bbece4a83b31c11b
unknown <pserra@mit.edu>
2020-11-06 18:01:27 -0500

    updated ipc packet class TX_PACKETS, added example

--- lib/ipc_packets.py
+++ lib/ipc_packets.py
@@ -1,13 +1,12 @@
 import struct
 
 class IpcPacket:
-    def __init__(self):
-        self.raw = ''
+    def __init__(self): pass
     
 class TxPacket:
-    def __init__(self):
+    def __init__(self): IpcPacket.__init__(self)
     
-    def encode(self,APID=0x01,payload ='')
+    def encode(self, APID=0x01, payload=''):
         '''Encode a packet to be transmited to the bus:
         APID: BCT APID presneted to the bus
         payload: raw contents, bytes
@@ -17,11 +16,14 @@ class TxPacket:
         self.size = len(payload)
         self.payload = payload
         
-        self.raw = struct.pack('cH%ds'%self.size,APID,self.size,payload)
+        if self.payload:
+            self.raw = struct.pack('BH%ds'%self.size,APID,self.size,payload)
+        else:
+            self.raw = struct.pack('BH',APID,self.size)
         
         return self.raw
         
-    def decode(raw):
+    def decode(self, raw):
         '''Decode a packet to be transmited to the bus:
         raw: message bytes to decode 
         returns APID: BCT APID presneted to the bus
@@ -30,15 +32,26 @@ class TxPacket:
         self.raw = raw
         raw_size = len(raw)-4
         
-        self.APID, self.size, self.payload = struct.unpack('cH%ds'%raw_size)
+        self.APID, self.size, self.payload = struct.unpack('BH%ds'%raw_size,raw)
         
-        assert self.size = raw_size
+        assert self.size == raw_size
         
         return self.APID, self.payload
         
     def __str__(self):
         if self.payload:
-            return 'IPC TX_PACKETS, APID:%02X, size:%d, payload:'
+            return 'IPC TX_PACKETS, APID:%02X, size:%d, payload:%X' % (self.APID, self.size, int.from_bytes(self.payload,''))
         else: 
+            return 'IPC TX_PACKETS, APID:%02X, size:%d' % (self.APID, self.size)
+ 
+if __name__ == '__main__':
+
+    empty_ipc_txpacket = TxPacket()
+    raw = empty_ipc_txpacket.encode(APID=0x02)
+    empty_ipc_txpacket.decode(raw)
+    print(empty_ipc_txpacket)
     
-    
+    not_empty_ipc_txpacket = TxPacket()
+    raw = not_empty_ipc_txpacket.encode(APID=0x02,payload=b"Je s'appelle Groot")
+    not_empty_ipc_txpacket.decode(raw)
+    print(not_empty_ipc_txpacket)
