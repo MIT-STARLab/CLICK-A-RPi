0ad3d9a5c8ccf4d57d0e50e6897ae7586330d095
gulbrillo <simon.barke@gmail.com>
2020-11-18 05:13:46 -0500

    test command line script acting as Bus Interface, commandhandler dealing with incoming Rx commands

--- commandhandler/commandhandler.py
+++ commandhandler/commandhandler.py
@@ -6,18 +6,19 @@ import sys
 import os
 import time
 import json
+import pickle
+import sys
 
-import sys #importing options and functions
+#importing options and functions
 sys.path.append('../lib/')
 sys.path.append('/home/pi/CLICK-A/github/lib/')
 from options import FPGA_MAP_ANSWER_PORT, FPGA_MAP_REQUEST_PORT, TX_PACKETS_PORT, RX_CMD_PACKETS_PORT, MESSAGE_TIMEOUT, PAT_CONTROL_PORT, TEST_RESPONSE_PORT
-
-
-## JSON alternative
-# print struct.unpack("i", struct.pack("i",4))[0] ?
+from ipc_packets import FPGAMapRequestPacket, FPGAMapAnswerPacket, TxPacket
+from zmqTxRx import recv_zmq, send_zmq
 
 # use PID as unique identifier for this progress
 topic = str(os.getpid())
+pid = os.getpid()
 
 # ZeroMQ inter process communication
 context = zmq.Context()
@@ -31,23 +32,21 @@ socket_test_response_packets.connect("tcp://localhost:%s" % TEST_RESPONSE_PORT) 
 socket_pat_control = context.socket(zmq.PUB) #send messages on this port
 socket_pat_control.connect("tcp://localhost:%s" % PAT_CONTROL_PORT) #connect to specific address (localhost)
 
-socket_FPGA_map_answer = context.socket(zmq.SUB)
-socket_FPGA_map_answer.connect ("tcp://localhost:%s" % FPGA_MAP_ANSWER_PORT)
 print ("Subscribing to FPGA_MAP_ANSWER topic {}".format(topic))
 print ("on port {}".format(FPGA_MAP_ANSWER_PORT))
+socket_FPGA_map_answer = context.socket(zmq.SUB)
 socket_FPGA_map_answer.setsockopt(zmq.SUBSCRIBE, topic.encode('ascii'))
 socket_FPGA_map_answer.setsockopt(zmq.RCVTIMEO, MESSAGE_TIMEOUT) # 5 second timeout on receive
+socket_FPGA_map_answer.connect ("tcp://localhost:%s" % FPGA_MAP_ANSWER_PORT)
 
 socket_tx_packets = context.socket(zmq.PUB)
 socket_tx_packets.connect ("tcp://localhost:%s" % TX_PACKETS_PORT)
 
-print ("Subscribing to all RX_CMD_PACKETS topics")
+print ("Pulling RX_CMD_PACKETS")
 print ("on port {}".format(RX_CMD_PACKETS_PORT))
-socket_rx_command_packets = context.socket(zmq.SUB)
-socket_rx_command_packets.connect ("tcp://localhost:%s" % RX_CMD_PACKETS_PORT)
-# socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-# subscribe to ALL incoming RX_CMD_PACKETS
-socket_rx_command_packets.setsockopt(zmq.SUBSCRIBE, b'')
+socket_rx_command_packets = context.socket(zmq.PULL)
+socket_rx_command_packets.connect ("tcp://127.0.0.1:%s" % RX_CMD_PACKETS_PORT)
+
 
 # socket needs some time to set up. give it a second - else the first message will be lost
 time.sleep(1)
@@ -82,18 +81,78 @@ def returnToTest(): #this should be the Bus
     print (test_response_packets)
     socket_test_response_packets.send(mogrify(topic, test_response_packets ))
 
+
+
 def getFPGAmap():
+    ipc_fpgarqpacket_write = FPGAMapRequestPacket()
+
+    '''
+    #create bytes object (struct) for writing (we are not doing this [yet])
+    raw = ipc_fpgarqpacket_write.encode(return_addr=pid, rq_number=123, rw_flag=1, start_addr=0x5678, size=48, write_data=b"number of characters must match 'size' attribute")
+    '''
+    #create bytes object (struct) for reading
+    '''do we need error handling for struct generation?'''
+    raw = ipc_fpgarqpacket_write.encode(return_addr=pid, rq_number=123, rw_flag=0, start_addr=0x5678, size=48)
+
+    #what's going on
+    print('')
+    ipc_fpgarqpacket_write.decode(raw)
+    print('PAYLOAD')
+    print(ipc_fpgarqpacket_write)
+    print('')
+
+    #send message
+    '''do we need the return_addr in the struct?? we DO need it here'''
+    send_zmq(socket_FPGA_map_request, raw, pid)
+
+    #what's going on
+    print('')
+    print('SENDING')
+    print(raw)
+    print('TO')
+    print(socket_FPGA_map_request.get_string(zmq.LAST_ENDPOINT))
+    print('WITH ENVELOPE')
+    print(pid)
+
+
+    #socket_FPGA_map_request.send(b'123' + b' ' + ipc_fpgarqpacket_write)
+
+    #send_pickle(socket_FPGA_map_request, ipc_fpgarqpacket_write, '123')
+
+    print("")
+    print("Waiting for FPGA_map_answer with topic {} ...".format(topic))
+    print("")
+
+    '''TODO: send to FPGA, wait for response, on timeout, send error'''
+
+    not_empty_ipc_txpacket = TxPacket()
+    raw = not_empty_ipc_txpacket.encode(APID=0x02,payload=b"timeout")
+    not_empty_ipc_txpacket.decode(raw)
+    print(not_empty_ipc_txpacket)
+
+    send_zmq(socket_tx_packets, raw, pid)
+
+    #what's going on
+    print('')
+    print('SENDING')
+    print(raw)
+    print('TO')
+    print(socket_tx_packets.get_string(zmq.LAST_ENDPOINT))
+    print('WITH ENVELOPE')
+    print(pid)
+
+    '''
     FPGA_map_request = dict()
     FPGA_map_request ['timestamp'] = time.time()
     FPGA_map_request ['command'] = 'gib'
     print ("Sending FPGA_map_request from process {}".format(topic))
     print (FPGA_map_request)
     socket_FPGA_map_request.send(mogrify(topic, FPGA_map_request ))
+    '''
 
-    print ("")
 
-    print ("Waiting for FPGA_map_answer with topic {} ...".format(topic))
 
+    '''
     try:
        recv_topic, FPGA_map_answer = demogrify(socket_FPGA_map_answer.recv())
        print (recv_topic, FPGA_map_answer)
@@ -105,10 +164,12 @@ def getFPGAmap():
            pass
        else:
            raise
+    '''
 
 print ("\n")
 
 while True:
+    '''
     try:
        print("Waiting for socket_rx_command_packets ...")
        recv_topic, rx_command_packets = demogrify(socket_rx_command_packets.recv())
@@ -122,8 +183,24 @@ while True:
            pass
        else:
            raise
+    '''
 
-    #getFPGAmap()
-    time.sleep(1)
+    #what's going on
+    print('')
+    print('RECEIVING ON')
+    print(socket_rx_command_packets.get_string(zmq.LAST_ENDPOINT))
+    print('WITH TIMEOUT')
+    print(socket_rx_command_packets.get(zmq.RCVTIMEO))
+
+    message = recv_zmq(socket_rx_command_packets)
+
+    #what's going on
+    print('')
+    print('GOT MESSAGE')
+    print(message)
+    print('WITH ENVELOPE')
+
+    getFPGAmap()
+    time.sleep(10)
 
 
