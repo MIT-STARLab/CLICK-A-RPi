74155ae6457663b5ef8c88e96ccfd26d71116a14
Peter <peterwgrenfell@gmail.com>
2020-12-03 16:37:48 -0500

    Clean up camera

--- camera/ipc/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Documents/CLICK-A-RPi/camera/ipc
-# Build directory: /home/pi/Documents/CLICK-A-RPi/camera/ipc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(camera-ipc "/home/pi/Documents/CLICK-A-RPi/camera/ipc/build/camera-ipc")
