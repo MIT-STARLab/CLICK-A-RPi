c18b970538eca29e2f0d1c62d3d76bdeb672a962
Ondrej Cierny <ondrej@mit.edu>
2020-12-04 18:01:28 -0500

    Added kernel SPI driver

---
+++ bus/driver/README.md
@@ -0,0 +1,43 @@
+# CLICK SPI driver
+Source for the kernel driver handling communication with the VNC2L over SPI.
+
+- `driver.h` contains some configuration parameters (SPI freq, max memory allocation, etc.) and type definitions
+- `driver.c` implements all SPI communication logic
+- `main.c` does kernel module init and handles queueing and reading of packets
+- `click_spi.dts` configures the RPi device tree to use the driver on SPI0 CE0 and sets the interrupt GPIO
+- `test_write.c` shows an example program used to write a telemetry packet to the driver
+
+## Packet exchange with user-space software
+The driver creates a new [character device](https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html) called `/dev/bct` which is used to read and write packets.
+
+Any process can open the device for writing (O_WRONLY) and write raw packets, which will be queued in memory using a [kernel linked list](https://www.kernel.org/doc/html/v4.14/core-api/kernel-api.html). Each packet must be written using a single write call. Then, every time the VNC2L triggers an interrupt, the driver will transmit the first packet in the list. The maximum queue memory size is set in `driver.h` via `MAX_TX_QUEUE_LEN` and has no upper limit apart from available RAM. Currently it's set to 2^26 bytes (67 MB). If the limit is reached, the write function will return -12 (ENOMEM).
+
+Only one process can open the device for reading (O_RDWR/O_RDONLY). The read calls are blocking, and will return when the requested amount of bytes is available. All SPI transfers append received data into a [kernel fifo](https://www.kernel.org/doc/htmldocs/kernel-api/kfifo.html) buffer and all `/dev/bct` reads will pull from this fifo. The fifo memory size is set via `RX_FIFO_LEN` and is limited to 2^22 bytes (4 MB) by kfifo implementation.
+
+## Hardware configuration
+The configurable HW parameters include the SPI frequency (via `SPI_FREQ`) and the GPIO pin which is used to handle the interrupt from VNC2L, set in `click_spi.dts`. The GPIO pin can be changed from its default (GPIO25) in `/boot/config.txt` by passing `dtoverlay=click_spi,irq=25`.
+
+## Building (without buildroot)
+There are two steps to build the driver:
+1. Compiling the kernel module
+2. Compiling the device tree overlay
+
+The module can be either cross-compiled on any linux host, or compiled directly on the RPi. This [guide](https://www.raspberrypi.org/documentation/linux/kernel/building.md) shows the necessary steps needed to prepare the build environment. Once the kernel is checked out, the necessary make commands are:
+```
+cp -r driver/ linux/
+cd linux
+KERNEL=kernel7
+make bcm2709_defconfig
+make prepare
+make modules_prepare
+cd driver
+make
+```
+
+When cross-compiling, all make commands need the `ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-` parameters.
+
+The `click_spi.dts` file has to be compiled using the [device-tree-compiler](https://packages.debian.org/buster/device-tree-compiler). The following command will build it:
+```
+dtc -O dtb -o click_spi.dtbo -b 0 -@ click_spi.dts
+```
+The output `click_spi.dtbo` has to be put into `/boot/overlays/`. To enable it, `dtoverlay=click_spi` has to be added to `/boot/config.txt`.
---
+++ bus/driver/driver.c
@@ -0,0 +1,131 @@
+/*
+** Filename: driver.c
+** Author: Ondrej
+** 
+** CLICK kernel SPI driver
+** Handles communication with the VNC2L
+*/
+
+#include "driver.h"
+
+/* Reinitialize SPI data structure */
+static void spi_data_init(spi_data_t *data)
+{
+    data->sync = 0;
+    data->read_len = 0;
+    data->payload_len = 0;
+    data->busy = true;
+    data->read_ptr = NULL;
+    data->header = NULL;
+    data->tx = list_first_entry_or_null(data->tx_queue, packet_t, list);
+}
+
+/* SPI transmit-receive function */
+static void spi_xfer(spi_data_t *data, uint8_t *rx, uint16_t len)
+{
+    data->xfer.tx_buf = data->tx ? data->tx->data : NULL;
+    data->xfer.rx_buf = rx;
+    data->xfer.len = len;
+    data->read_len += len;
+    data->read_ptr = rx;
+    if(spi_async(data->spi, &data->msg) != 0)
+    {
+        dev_warn_once(&data->spi->dev, "spi_async error\n");
+    }
+}
+
+/* Main VNC2L interrupt handler */
+irqreturn_t interrupt_handler(int irq, void *arg)
+{
+    spi_data_t *data = arg;
+    if (data->busy == false)
+    {
+        spi_data_init(data);
+        if (data->tx != NULL)
+        {
+            list_del(&data->tx->list);
+            spi_xfer(data, data->rx, data->tx->len);
+        }
+        else spi_xfer(data, data->rx, DEFAULT_READ_LEN);
+    }
+    dev_notice_once(&data->spi->dev, "IRQ is working\n");
+    return IRQ_HANDLED;
+}
+
+/* SPI completion handler */
+void spi_xfer_complete(void *arg)
+{
+    unsigned int copied = 0;
+    spi_data_t *data = arg;
+
+    /* Free tx packet memory, if any */
+    if (data->tx)
+    {
+        packet_free(data->tx);
+        data->tx = NULL;
+    }
+    
+    /* Start parsing rx data */
+    while(data->read_len)
+    {
+        /* Waiting for sync marker */
+        if (!SYNC_VALID(data->sync))
+        {
+            data->read_len--;
+            SYNC_UPDATE(data->sync, data->read_ptr++);
+            if (SYNC_VALID(data->sync))
+            {
+                data->header = (packet_header_t*) data->read_ptr;
+                /* Request more header data if needed */
+                if(data->read_len < PACKET_HEADER_LEN)
+                {
+                    data->read_ptr += data->read_len;
+                    spi_xfer(data, data->read_ptr, PACKET_HEADER_LEN - data->read_len);
+                    return;
+                }
+            }
+        }
+        /* After sync, read CCSDS header */
+        else if (data->payload_len == 0)
+        {
+            data->payload_len = ((data->header->len_msb << 8) | data->header->len_lsb) + 1;
+            data->read_ptr += PACKET_HEADER_LEN;
+            data->read_len -= PACKET_HEADER_LEN;
+            /* Request more payload data if needed */
+            if(data->read_len < data->payload_len)
+            {
+                data->read_ptr += data->read_len;
+                spi_xfer(data, data->read_ptr, data->payload_len - data->read_len);
+                return;
+            }
+        }
+        /* Finish reading packet */
+        else
+        {
+            data->read_ptr += data->read_len;
+            data->read_len = 0;
+        }
+    }
+
+    /* Check if successful */
+    if (data->payload_len > 0)
+    {
+        /* Try adding packet to fifo */
+        if(kfifo_avail(data->rx_fifo) >= (PACKET_OVERHEAD + data->payload_len))
+        {
+            copied = kfifo_in(data->rx_fifo, ((uint8_t*) data->header) - PACKET_SYNC_LEN,
+                PACKET_OVERHEAD + data->payload_len);
+            if (copied == (PACKET_OVERHEAD + data->payload_len))
+            {
+                dev_notice_once(&data->spi->dev, "packets are being received\n");
+            }
+            else dev_warn_once(&data->spi->dev, "rx fifo copy error\n");
+        }
+        else dev_warn_once(&data->spi->dev, "rx fifo is full\n");
+    }
+    else dev_warn_once(&data->spi->dev, "invalid packet received\n");
+
+    /* Clear busy flag and wake up read function */
+    data->busy = false;
+    complete(data->xfer_done);
+}
