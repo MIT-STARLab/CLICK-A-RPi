2c95409067f866adf79b52137301722db2b711a5
Peter Grenfell <pgrenfel@mit.edu>
2020-12-14 20:05:38 -0500

    add argparse to fpga-ipc

--- fpga/fpga-ipc.py
+++ fpga/fpga-ipc.py
@@ -5,6 +5,7 @@ import zmq
 import json
 import time
 import fl
+import argparse
 
 import sys #importing options and functions
 sys.path.append('../lib/')
@@ -42,8 +43,30 @@ def demogrify(topicmsg):
     msg = json.loads(topicmsg[json0:])
     return topic, msg
 
-
-print ("\n")
+print("NODE FPGA Commander Copyright (C) 2015 Ryan Kingsbury\n")
+parser = argparse.ArgumentParser(description='Load FX2LP firmware, load the FPGA, interact with the FPGA.')
+parser.add_argument('-i', action="store", nargs=1, metavar="<VID:PID>", help="vendor ID and product ID (e.g 1443:0007)")
+parser.add_argument('-v', action="store", nargs=1, required=True, metavar="<VID:PID>", help="VID, PID and opt. dev ID (e.g 1D50:602B:0001)")
+#parser.add_argument('-d', action="store", nargs=1, metavar="<port+>", help="read/write digital ports (e.g B13+,C1-,B2?)")
+parser.add_argument('-q', action="store", nargs=1, metavar="<jtagPorts>", help="query the JTAG chain")
+parser.add_argument('-p', action="store", nargs=1, metavar="<config>", help="program a device")
+parser.add_argument('-c', action="store", nargs=1, metavar="<conduit>", help="which comm conduit to choose (default 0x01)")
+parser.add_argument('-f', action="store", nargs=1, metavar="<dataFile>", help="binary data to write to channel 0")
+parser.add_argument('-N', action="store", nargs=1, metavar="numwrites", help="Number of times to write file to FPGA")
+parser.add_argument('--ppm', action="store", nargs=1, metavar="<M>", help="PPM order")
+parser.add_argument('--txdel', action="store", nargs=1, metavar="<delay>", help="TX loopback delay (in clock cycles)")
+parser.add_argument('--dac', action="store", nargs=1, metavar="<counts>", help="DAC setting")
+parser.add_argument('--prbs', action="store", nargs='?',const=True, metavar="<prbs>", help="Use PRBS")
+parser.add_argument('--ser', action="store", nargs='?',const='1', metavar="<dwell>", help="perform slot error rate measurement")
+parser.add_argument('--serCont', action="store", nargs='?',const='1', metavar="<dwell>", help="perform continuous slot error rate measurement")
+parser.add_argument('--peak', action="store", nargs='?',const='0.1', metavar="<dwell>", help="peak power measurement, binary search for DAC value")
+parser.add_argument('--read', action="store", nargs=1, metavar="<regnum>", help="Reads from the FPGA memory map, given a channel number (0-127)", type=int)
+parser.add_argument('--write', action="store", nargs=2, metavar="<regnum>", help="Writes to the FPGA memory map, given a channel number (0-127) and value to be written", type=int)
+parser.add_argument('--edfa', action="store", nargs=1, metavar="<edfacmd>", help="Writes either edfa on, edfa off, or flash to the FPGA register controlling the edfa")
+parser.add_argument('--send', action="store", nargs=2, metavar="<sendfile>", help="Sends a binary file to the FPGA Modulator along with desired PPM order")
+parser.add_argument('--power', action="store", nargs=1, metavar="<power>", help="Turns power on/off on the FPGA board")
+parser.add_argument('--update', nargs = '?', help="Starts printing the values in registers 96-117 every 0.1 seconds until told to stop")
+parser.add_argument('--interval', action="store", nargs=1, metavar="<interval>", help="Sets the printing interval", type=int)
 
 argList = parser.parse_args()
 
@@ -81,7 +104,7 @@ except fl.FLException as ex:
 finally:
     fl.flClose(handle)
 
-
+print ("\n")
 
 while True:
 
