52275c921960166987e582b58dfacb717c0d0581
Hannah Tomio <tomio@mit.edu>
2021-01-07 02:57:06 -0600

    Add simple file chunking (untested)

--- commandhandler/commandhandler.py
+++ commandhandler/commandhandler.py
@@ -9,7 +9,8 @@ import json
 import pickle
 import sys
 import struct
-
+import math
+import hashlib
 #importing options and functions
 sys.path.append('../lib/')
 sys.path.append('/home/pi/CLICK-A/github/lib/')
@@ -111,23 +112,23 @@ while True:
 
     # Parse received command payload as per control packet format (TBR)
     generic_control_packet = GenericControlPacket()
-    generic_control_packet.decode(ipc_rxcompacket.payload)     
+    generic_control_packet.decode(ipc_rxcompacket.payload)
     CMD_ID = generic_control_packet.command
-    
+
     if(CMD_ID == CMD_PL_REBOOT):
-        #TODO: Acknowledge command receipt via telemetry       
+        #TODO: Acknowledge command receipt via telemetry
         os.system("sudo shutdown -r now") #reboot the RPi (TODO: Add other shutdown actions if needed)
-        
+
     elif(CMD_ID == CMD_PL_ENABLE_TIME):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_DISABLE_TIME):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_EXEC_FILE):
-        #TODO: Acknowledge command receipt via telemetry 
+        #TODO: Acknowledge command receipt via telemetry
         raw_size = generic_control_packet.size - 3 #TBR
         output_to_file, file_out_num, file_path_len, raw_file_path = struct.unpack('BBB%ds'%raw_size,generic_control_packet.payload) #TBR
         file_path = raw_file_path.decode('utf-8') #TBR
@@ -136,14 +137,14 @@ while True:
             os.system(file_path) #TBR
             #if(output_to_file):
                 #TODO - have the script write to file with file_out_num (= file output number)
-                
+
             #TODO: Send success telemetry
         except:
             #TODO: Send error telemetry
             pass
-        
+
     elif(CMD_ID == CMD_PL_LIST_FILE):
-        #TODO: Acknowledge command receipt via telemetry 
+        #TODO: Acknowledge command receipt via telemetry
         raw_size = generic_control_packet.size - 1 #TBR
         directory_path_len, raw_directory_path = struct.unpack('B%ds'%raw_size,generic_control_packet.payload) #TBR
         directory_path = raw_directory_path.decode('utf-8') #TBR
@@ -154,7 +155,7 @@ while True:
             return_data = ''
             for list_item in directory_listing:
                 return_data = return_data + list_item + '\n' #TBR separator type
-                
+
             list_file_txpacket = TxPacket()
             raw = list_file_txpacket.encode(payload=return_data) #TBR
 
@@ -162,42 +163,68 @@ while True:
             print(list_file_txpacket) #Debug printing
             print ('SENDING to %s' % (socket_tx_packets.get_string(zmq.LAST_ENDPOINT))) #Debug printing
             print(raw) #Debug printing
-            
+
             socket_tx_packets.send(raw) #send packet
-                
+
             #TODO: Send success telemetry
         except:
             #TODO: Send error telemetry
             pass
-            
+
     elif(CMD_ID == CMD_PL_REQUEST_FILE):
-        #TODO
-        pass
-        
+        # simple dumb file chunking
+        max_chunk_size = 4068
+        file_name = generic_control_packet.payload #TBR
+        with open(file_name, "rb") as source_file:
+        	file_id = hashlib.md5(file_name).digest()
+        	file_len = os.stat(file_name).st_size
+        	seq_len = math.ceil(float(file_len)/max_chunk_size)
+        	seq_cnt = 0
+        	while ((seq_cnt + 1)*max_chunk_size) < file_len:
+        		packet_payload = source_file.read(max_chunk_size)
+        		packet = struct.pack('%dsHH%ds' % (16, max_chunk_size), file_id, seq_cnt, seq_len, packet_payload)
+
+        		txpacket = TxPacket()
+        		raw_packet = txpacket.encode(payload=packet)
+        		socket_tx_packets.send(raw_packet)
+
+        		seq_cnt += 1
+
+        	if (seq_cnt*max_chunk_size < file_len):
+        		packet_data_len = file_len - (seq_cnt * max_chunk_size)
+        		packet_payload = source_file.read(packet_data_len)
+        		packet = struct.pack('%dsHH%ds' % (16, packet_data_len), file_id, seq_cnt, seq_len, packet_payload)
+
+        		# print(len(packet))
+        		# print(binascii.hexlify(packet))
+        		txpacket = TxPacket()
+        		raw_packet = txpacket.encode(payload=packet)
+        		socket_tx_packets.send(raw_packet)
+
     elif(CMD_ID == CMD_PL_UPLOAD_FILE):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_SINGLE_CAPTURE):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_RUN_CALIBRATION):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_SET_FPGA):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_GET_FPGA):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_SET_HK):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_ECHO):
         echo_txpacket = TxPacket()
         raw = echo_txpacket.encode(payload=generic_control_packet.payload) #TBR
@@ -206,28 +233,28 @@ while True:
         print(echo_txpacket) #Debug printing
         print ('SENDING to %s' % (socket_tx_packets.get_string(zmq.LAST_ENDPOINT))) #Debug printing
         print(raw) #Debug printing
-        
+
         socket_tx_packets.send(raw) #send packet
-        
+
     elif(CMD_ID == CMD_PL_NOOP):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_DWNLINK_MODE):
         #TODO
         pass
-        
+
     elif(CMD_ID == CMD_PL_DEBUG_MODE):
         #TODO
         pass
-        
+
     else: #default
         #TODO
         pass
-        
+
     # ~ if ipc_rxcompacket.payload == b'block':
         # ~ print ("SLEEPING FOR 60 SECONDS")
         # ~ time.sleep(30)
     # ~ elif ipc_rxcompacket.payload == b'getMap':
         # ~ print ("REQUESTING FPGA MAP")
-        # ~ getFPGAmap() 
+        # ~ getFPGAmap()
