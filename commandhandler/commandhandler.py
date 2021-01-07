26176ff6ee0f1988452f605b2dfb54952ff9fd12
Peter <peterwgrenfell@gmail.com>
2021-01-06 22:06:00 -0500

    update command list and start adding to commandhandler

--- commandhandler/commandhandler.py
+++ commandhandler/commandhandler.py
@@ -53,12 +53,12 @@ socket_rx_command_packets.connect ("tcp://127.0.0.1:%s" % RX_CMD_PACKETS_PORT)
 # socket needs some time to set up. give it a second - else the first message will be lost
 time.sleep(1)
 
-def getFPGAmap():
+def getFPGAmap(request_number, num_registers, start_address):
     ipc_fpgarqpacket_read = FPGAMapRequestPacket()
 
     #create bytes object (struct) for reading
     '''do we need error handling for struct generation?'''
-    raw = ipc_fpgarqpacket_read.encode(return_addr=pid, rq_number=123, rw_flag=0, start_addr=0x5678, size=48)
+    raw = ipc_fpgarqpacket_read.encode(return_addr=pid, rq_number=request_number, rw_flag=0, start_addr=start_address, size=num_registers)
     ipc_fpgarqpacket_read.decode(raw)
 
     print ('SENDING to %s with ENVELOPE %d' % (socket_rx_command_packets.get_string(zmq.LAST_ENDPOINT), ipc_fpgarqpacket_read.return_addr))
@@ -109,14 +109,125 @@ while True:
     print (ipc_rxcompacket)
     print ('| got PAYLOAD %s' % (ipc_rxcompacket.payload))
 
-    # do something with the package
-    if ipc_rxcompacket.payload == b'block':
-        print ("SLEEPING FOR 60 SECONDS")
-        time.sleep(30)
-    elif ipc_rxcompacket.payload == b'getMap':
-        print ("REQUESTING FPGA MAP")
-        getFPGAmap()
+    # Parse received command payload as per control packet format (TBR)
+    generic_control_packet = GenericControlPacket()
+    generic_control_packet.decode(ipc_rxcompacket.payload)     
+    CMD_ID = generic_control_packet.command
+    
+    if(CMD_ID == CMD_PL_REBOOT):
+        #TODO: Acknowledge command receipt via telemetry       
+        os.system("sudo shutdown -r now") #reboot the RPi (TODO: Add other shutdown actions if needed)
+        
+    elif(CMD_ID == CMD_PL_ENABLE_TIME):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_DISABLE_TIME):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_EXEC_FILE):
+        #TODO: Acknowledge command receipt via telemetry 
+        raw_size = generic_control_packet.size - 3 #TBR
+        output_to_file, file_out_num, file_path_len, raw_file_path = struct.unpack('BBB%ds'%raw_size,generic_control_packet.payload) #TBR
+        file_path = raw_file_path.decode('utf-8') #TBR
+        file_path = file_path[0:(file_path_len-1)] #Strip padding
+        try:
+            os.system(file_path) #TBR
+            #if(output_to_file):
+                #TODO - have the script write to file with file_out_num (= file output number)
+                
+            #TODO: Send success telemetry
+        except:
+            #TODO: Send error telemetry
+            pass
+        
+    elif(CMD_ID == CMD_PL_LIST_FILE):
+        #TODO: Acknowledge command receipt via telemetry 
+        raw_size = generic_control_packet.size - 1 #TBR
+        directory_path_len, raw_directory_path = struct.unpack('B%ds'%raw_size,generic_control_packet.payload) #TBR
+        directory_path = raw_directory_path.decode('utf-8') #TBR
+        directory_path = directory_path[0:(directory_path_len-1)] #Strip padding
+        try:
+            directory_listing = os.listdir(directory_path) #Get directory list
+            #Convert list to single string for telemetry:
+            return_data = ''
+            for list_item in directory_listing:
+                return_data = return_data + list_item + '\n' #TBR separator type
+                
+            list_file_txpacket = TxPacket()
+            raw = list_file_txpacket.encode(payload=return_data) #TBR
 
+            list_file_txpacket.decode(raw) #for Debug printing
+            print(list_file_txpacket) #Debug printing
+            print ('SENDING to %s' % (socket_tx_packets.get_string(zmq.LAST_ENDPOINT))) #Debug printing
+            print(raw) #Debug printing
+            
+            socket_tx_packets.send(raw) #send packet
+                
+            #TODO: Send success telemetry
+        except:
+            #TODO: Send error telemetry
+            pass
+            
+    elif(CMD_ID == CMD_PL_REQUEST_FILE):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_UPLOAD_FILE):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_SINGLE_CAPTURE):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_RUN_CALIBRATION):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_SET_FPGA):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_GET_FPGA):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_SET_HK):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_ECHO):
+        echo_txpacket = TxPacket()
+        raw = echo_txpacket.encode(payload=generic_control_packet.payload) #TBR
 
-
-
+        echo_txpacket.decode(raw) #for Debug printing
+        print(echo_txpacket) #Debug printing
+        print ('SENDING to %s' % (socket_tx_packets.get_string(zmq.LAST_ENDPOINT))) #Debug printing
+        print(raw) #Debug printing
+        
+        socket_tx_packets.send(raw) #send packet
+        
+    elif(CMD_ID == CMD_PL_NOOP):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_DWNLINK_MODE):
+        #TODO
+        pass
+        
+    elif(CMD_ID == CMD_PL_DEBUG_MODE):
+        #TODO
+        pass
+        
+    else: #default
+        #TODO
+        pass
+        
+    # ~ if ipc_rxcompacket.payload == b'block':
+        # ~ print ("SLEEPING FOR 60 SECONDS")
+        # ~ time.sleep(30)
+    # ~ elif ipc_rxcompacket.payload == b'getMap':
+        # ~ print ("REQUESTING FPGA MAP")
+        # ~ getFPGAmap() 
