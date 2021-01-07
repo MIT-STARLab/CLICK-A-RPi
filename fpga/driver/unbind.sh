e7d0c0fd4811062859990474e026cf2b19e0ce31
Ondrej Čierny <cierny@users.noreply.github.com>
2021-01-07 15:43:45 -0500

    Update unbind.sh

--- fpga/driver/unbind.sh
+++ fpga/driver/unbind.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if grep -c 'Driver=(none)' /sys/kernel/debug/usb/devices
-then
-        echo "Linux usbfs binding does not exist"
-else
-	ExecStartPre=/sys/bus/usb/devices/1-1.2:1.0/driver/ "echo 1-1.2:1.0 > unbind"
+cnt=$(grep -c 'Driver=usbfs' /sys/kernel/debug/usb/devices)
+if [ $cnt -gt 0 ]; then
+        ExecStartPre=/sys/bus/usb/devices/1-1.2:1.0/driver/ "echo 1-1.2:1.0 > unbind"
         echo "Removed linux usbfs binding"
+else
+	echo "Linux usbfs binding does not exist"
 fi
