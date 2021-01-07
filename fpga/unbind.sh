345665b3b2f8c6f855121df3bc2d0f48bd6632aa
Grant Gunnison <gunnison@mit.edu>
2021-01-07 16:36:08 -0500

    new fpga folder and unbind update

--- fpga/unbind.sh
+++ fpga/unbind.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cnt=$(grep -c 'Driver=usbfs' /sys/kernel/debug/usb/devices)
-if [ $cnt -gt 0 ]; then
-        ExecStartPre=/sys/bus/usb/devices/1-1.2:1.0/driver/ "echo 1-1.2:1.0 > unbind"
-        echo "Removed linux usbfs binding"
+if [ $cnt -eq 0 ]; then
+        echo "Linux usbfs binding does not exist"
 else
-	echo "Linux usbfs binding does not exist"
+	ExecStartPre=/sys/bus/usb/devices/1-1.2:1.0/driver/ "echo 1-1.2:1.0 > unbind"
+        echo "Removed linux usbfs binding"
 fi
