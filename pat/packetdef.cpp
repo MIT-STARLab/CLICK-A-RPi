70adb692772cbe82ce4682882f0db1db7412eeca
Peter Grenfell <pgrenfel@mit.edu>
2021-01-14 16:46:37 -0500

    update answer checking

--- pat/packetdef.cpp
+++ pat/packetdef.cpp
@@ -197,10 +197,16 @@ bool check_fpga_map_write_request(zmq::socket_t& fpga_map_answer_port, std::vect
 			// received something on the first (only) socket
 			fpga_answer_struct write_ans_struct = receive_packet_fpga_map_answer(fpga_map_answer_port, WRITE);
 			//make sure message is for PAT process:
+			std::cout << "In packedef.cpp - check_fpga_map_write_request: Response Attempt = " << i << std::endl;
+			std::cout << "In packedef.cpp - check_fpga_map_write_request: return_address (Tx) = " << (uint32_t) getpid() << std::endl;
+			std::cout << "In packedef.cpp - check_fpga_map_write_request: return_address (Rx) = " << write_ans_struct.return_address << std::endl;
+			std::cout << "In packedef.cpp - check_fpga_map_write_request: request_number (Tx) = " << request_number << std::endl;
+			std::cout << "In packedef.cpp - check_fpga_map_write_request: request_number (Rx) = " << write_ans_struct.request_number << std::endl;
 			if((((uint32_t) getpid()) == write_ans_struct.return_address) && (request_number == write_ans_struct.request_number)){
 				return !write_ans_struct.error_flag;
 			} 
 		}
 	}
+	std::cout << "In packedef.cpp - check_fpga_map_write_request: FPGA MAP Check Timeout!" << std::endl; 
 	return false; //timeout
 }
