b2eb99ab74cde8b1d52bcfaea2695f3b17a7f6b6
Peter Grenfell <pgrenfel@mit.edu>
2021-01-14 19:31:42 -0500

    fix track bug

--- pat/tracking.h
+++ pat/tracking.h
@@ -50,14 +50,14 @@ class Tracking
 	std::ofstream &fileStream;
 	zmq::socket_t &pat_health_port;
 	zmq::socket_t &pat_control_port;
-	std::vector<zmq::pollitem_t>& poll_pat_control;
-	bool received_end_pat_cmd = false; 
+	std::vector<zmq::pollitem_t>& poll_pat_control; 
 	bool verifyFrame(Image& frame);
 	bool windowAndTune(Image& frame, Group& beacon);
 	bool autoTuneExposure(Group& beacon);
 public:
 	int beaconSmoothing;
 	double actionX, actionY;
+	bool received_end_pat_cmd = false;
 	Tracking(Camera& c, Calibration& calib, std::ofstream &fileStreamIn, zmq::socket_t &pat_health_port_in, zmq::socket_t& pat_control_port_in, std::vector<zmq::pollitem_t>& poll_pat_control_in) : camera(c), calibration(calib), fileStream(fileStreamIn), pat_health_port(pat_health_port_in), pat_control_port(pat_control_port_in), poll_pat_control(poll_pat_control_in), beaconSmoothing(0), actionX(0), actionY(0) {};
 	bool runAcquisition(Group& beacon);
 	int findSpotCandidate(Image& frame, Group& oldSpot, double *difference);
