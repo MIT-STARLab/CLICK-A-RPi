fe2b63be931570878cecf55b229bbbb15acefd42
Grant Gunnison <gunnison@mit.edu>
2021-01-16 18:33:11 -0500

    bist settings code

--- fpga/dac_spi.py
+++ fpga/dac_spi.py
@@ -1,14 +1,13 @@
-# fsm.py
-# Joe Kusters
-# helper functions for writing to FSM DAC (AD5664R)
-import fl
+import sys
+sys.path.append('/root/fpga/')
+import spi
 from time import sleep
 
 #PD DAC = "PD"
 #CALIBRATION laser channel 4 on PD DAC
 #FSM DAC = FSM
 
-def init(handle, dac):
+def init(dac):
     reg = []
     #pd_dac
     if(dac == "PD"):
@@ -18,81 +17,80 @@ def init(handle, dac):
         reg = [8,9,10]
     #none
     else:
-        reg = [0,0,0]	
+        print("Invalid DAC, use PD or FSM")	
+        reg = [0,0,0]
 
-    reset(handle,reg)
+    reset(reg)
     sleep(0.1)
-    enable_int_ref(handle,reg)
+    enable_int_ref(reg)
     sleep(0.1)
-    enable_all_dacs(handle,reg)
+    enable_all_dacs(reg)
     sleep(0.1)
-    enable_sw_ldac(handle,reg)
+    enable_sw_ldac(reg)
     sleep(0.1)
     return
 
 #Resets registers to 0: 
 #register 7 value 1: DAC, Input, LDAC, Power-down, internal reference setup
 #register 7 value 0: DAC register & Input Shift register only
-def reset(handle,dac):
-    print(dac)
-    fl.flWriteChannel(handle, dac[0], 0x28)
+def reset(dac):
+    spi.write_register(dac[0], 0x28)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[1], 0)
+    spi.write_register(dac[1], 0)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[2], 0x01)
+    spi.write_register(dac[2], 0x01)
     return
 #internal reference on register 7 value 1 = on, 0 = off
-def enable_int_ref(handle,dac):
-    print(dac)
-    fl.flWriteChannel(handle, dac[0], 0x38)
+def enable_int_ref(dac):
+    spi.write_register(dac[0], 0x38)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[1], 0)
+    spi.write_register(dac[1], 0)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[2], 0x01)
+    spi.write_register(dac[2], 0x01)
     return
 
 #Powers up all DAC's to normal operation mode
 #Setting DB4&5 can fix the output impedance of the DAC
-def enable_all_dacs(handle,dac):
-    print(dac)
-    fl.flWriteChannel(handle, dac[0], 0x20)
+def enable_all_dacs(dac):
+    spi.write_register(dac[0], 0x20)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[1], 0)
+    spi.write_register(dac[1], 0)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[2], 0x0F)
+    spi.write_register(dac[2], 0x0F)
     return
 #latch outputs to current register value 
 #input register will not affect the output
 #changing DB3-0 will update dac registers when new data is read in
-def enable_sw_ldac(handle,dac):
-    print(dac)
-    fl.flWriteChannel(handle, dac[0], 0x30)
+def enable_sw_ldac(dac):
+    spi.write_register(dac[0], 0x30)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[1], 0)
+    spi.write_register(dac[1], 0)
     sleep(0.1)
-    fl.flWriteChannel(handle, dac[2], 0)
+    spi.write_register(dac[2], 0)
     return
 #write to single register N
-def write_dac(handle, dac, addr, value):
+def write_dac(dac, addr, value):
     reg = []
     #pd_dac
     if(dac == "PD"):
     	reg = [5,6,7]
     #fsm_dac	
     elif (dac == "FSM"):
-	reg = [8,9,10]
+	    reg = [8,9,10]
     #none
     else:
-	reg = [0,0,0]	
+        print("Invalid DAC, use PD or FSM")
+        reg = [0,0,0]
 
-    fl.flWriteChannel(handle, reg[0], addr&0x7)
+    spi.write_register(reg[0], addr&0x7)
     sleep(0.1)
-    fl.flWriteChannel(handle, reg[1], (value&0xFF00)>>8)
+    spi.write_register(reg[1], (value&0xFF00)>>8)
     sleep(0.1)
-    fl.flWriteChannel(handle, reg[2], value&0xFF)
+    spi.write_register(reg[2], value&0xFF)
     return
+
 #updates all DAC's to current input values
-def update_dac(handle, dac):
+def update_dac(dac):
     reg = []
     #pd_dac
     if(dac == "PD"):
@@ -102,11 +100,12 @@ def update_dac(handle, dac):
     	reg = [8,9,10]
     #none
     else:
-	reg = [0,0,0]	
+        print("Invalid DAC, use PD or FSM")	
+        reg =[0,0,0]
 
-    fl.flWriteChannel(handle, reg[0], 0x0F)
+    spi.write_register(reg[0], 0x0F)
     sleep(0.1)
-    fl.flWriteChannel(handle, reg[1], 0)
+    spi.write_register(reg[1], 0)
     sleep(0.1)
-    fl.flWriteChannel(handle, reg[2], 0)
+    spi.write_register(reg[2], 0)
     return
