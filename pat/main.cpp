09f54d850b5f9fdd15317c05496f33bcaaf49840
Peter Grenfell <pgrenfel@mit.edu>
2021-01-28 19:49:01 -0500

    simplify

--- pat/main.cpp
+++ pat/main.cpp
@@ -191,9 +191,9 @@ int main() //int argc, char** argv
 	bool static_pointing_initialized = false;
 	
 	//IPC with fpga.py - TEST
-	std::cout << "command heater on" << std::endl;
-	send_packet_fpga_map_request(fpga_map_request_port, (uint16_t) HEATER_CH, (uint8_t) HEATER_ON, (bool) WRITE, 0);
-	std::cout << "check heater command: " << check_fpga_map_write_request(fpga_map_answer_port, poll_fpga_answer, (uint16_t) HEATER_CH, 0) << std::endl;
+	//std::cout << "command heater on" << std::endl;
+	//send_packet_fpga_map_request(fpga_map_request_port, (uint16_t) HEATER_CH, (uint8_t) HEATER_ON, (bool) WRITE, 0);
+	//std::cout << "check heater command: " << check_fpga_map_write_request(fpga_map_answer_port, poll_fpga_answer, (uint16_t) HEATER_CH, 0) << std::endl;
 	std::cout << "check heater on: " << check_fpga_map_value(fpga_map_answer_port, poll_fpga_answer, fpga_map_request_port, (uint16_t) HEATER_CH, (uint8_t) HEATER_ON, 1) << std::endl;
 	return 0; 
 
