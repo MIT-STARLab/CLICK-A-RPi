f8de88c97ea7fb61067d55a2a3568c612c0abab7
Hannah Tomio <tomio@mit.edu>
2021-02-12 22:02:31 -0500

    Add FPGA telemetry to housekeeping

--- housekeeping/housekeeping.py
+++ housekeeping/housekeeping.py
@@ -18,6 +18,7 @@ sys.path.append('../lib/')
 import ipc_helper
 from ipc_packets import TxPacket, CHHeartbeatPacket, FPGAMapRequestPacket, FPGAMapAnswerPacket, HKControlPacket, PATControlPacket, PATHealthPacket
 from options import *
+from fpga_map import FPGA_TELEM, FPGA_TELEM_TYPE
 from zmqTxRx import push_zmq, send_zmq, recv_zmq
 
 HK_PAT_ID = 0xA0
@@ -184,21 +185,23 @@ class Housekeeping:
             fpga_thread.start()
 
     def fpga_read(self):
-        # TODO: Update this with whatever the actual fpga telemetry should be
         try:
-            read = self.fpga_interface.read_reg(200, 4)
+            read = self.fpga_interface.read_reg(FPGA_TELEM_TYPE)
             self.fpga_queue.put(read)
         except error as e:
             self.alert_missing_fpga()
 
     def check_fpga(self, answer_pkt):
-        pkt = []
+        pkt = ''
 
         # 0: HK counter
-        pkt.extend(struct.pack('B', self.fpga_hk_count % 256))
-        # 1-N:
-        # TODO: Format packet properly if necessary
-        pkt.extend(answer_pkt)
+        pkt += struct.pack('B', self.fpga_hk_count % 256))
+
+        # 1-244: FPGA Telemetry Packet
+        # Handle FPGA packet format
+        for i in range(0, len(answer_pkt)):
+            pkt += struct.pack(FPGA_TELEM_TYPE[i], answer_pkt[i])
+        return pkt
 
     def check_sys(self):
         pkt = ''
@@ -273,6 +276,7 @@ class Housekeeping:
                 pkt += struct.pack('B', self.procs[p_name])
                 pkt += struct.pack('B', (p.cpu_percent() % 256))
                 pkt += struct.pack('B', (p.memory_percent('rss') % 256))
+        return pkt
 
 
     def handle_hk_pkt(self, data, process_id):
@@ -287,7 +291,7 @@ class Housekeeping:
 
         elif (process_id == HK_FPGA_ID):
             apid = TLM_HK_FPGA_MAP
-            payload = struct.pack('B', data) #TBR if the data is already a packed byte string or not. FPGA packet definition beyond 1 byte HK counter is TBD
+            payload = struct.pack('B', data)
             print('Handling FPGA pkt w/ payload: ', payload)
 
         elif (process_id == HK_SYS_ID):
@@ -322,7 +326,7 @@ class Housekeeping:
         self.packet_buf.append(raw_pkt)
 
     def handle_hk_command(self, command):
-        flags, new_fpga_check_pd, new_sys_check_pd, new_ch_heartbeat_pd, new_pat_health_pd = struct.unpack('!%BBBBB', command)
+        flags, new_fpga_check_pd, new_sys_check_pd, new_ch_heartbeat_pd, new_pat_health_pd = struct.unpack('!BBBBB', command)
 
         self.all_pkts_send_enable = (flags >> 7) & 1
         self.fpga_req_enable = (flags >> 6) & 1
