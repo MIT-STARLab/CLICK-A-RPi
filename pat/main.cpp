8ce747878fc364b150e266915a407f0aaa58578d
Peter Grenfell <pgrenfel@mit.edu>
2021-02-17 18:32:01 -0500

    fix syntax

--- pat/main.cpp
+++ pat/main.cpp
@@ -115,7 +115,6 @@ struct tx_offsets{
 };
 void calculateTxOffsets(zmq::socket_t& pat_health_port, std::ofstream& fileStream, zmq::socket_t& fpga_map_request_port, zmq::socket_t& fpga_map_answer_port, std::vector<zmq::pollitem_t>& poll_fpga_answer, tx_offsets& offsets){
 	fpga_answer_temperature_struct temperature_packet = fpga_answer_temperature_struct();
-	tx_offsets offsets = tx_offsets();
 	if(get_temperature(fpga_map_answer_port, poll_fpga_answer, fpga_map_request_port, temperature_packet, (uint16_t) TEMPERATURE_CH, 0)){
 		offsets.x = (int) TX_OFFSET_SLOPE_X*temperature_packet.temperature + TX_OFFSET_BIAS_X;
 		offsets.y = (int) TX_OFFSET_SLOPE_Y*temperature_packet.temperature + TX_OFFSET_BIAS_Y;
